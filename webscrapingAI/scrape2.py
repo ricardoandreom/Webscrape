da2e98069cb8c2abc3349e2d3a2a5b3927d158dc
Ricardo André <114098654+ricardoandreom@users.noreply.github.com>
2024-10-01 23:30:48 +0100

    Add files via upload

---
+++ webscrapingAI/scrape2.py
@@ -0,0 +1,52 @@
+import time
+
+from bs4 import BeautifulSoup
+import selenium.webdriver as webdriver
+from selenium.webdriver.chrome.service import Service
+
+
+def scrape_website(website):
+    print("Connecting to Scraping Browser...")
+    chrome_driver_path = "C:\\Users\\Admin\\Desktop\\chromedriver.exe"
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
+
+    try:
+        driver.get(website)
+        print("Page loaded...")
+        html = driver.page_source
+        time.sleep(10)
+
+        return html
+
+    finally:
+        driver.quit()
+
+
+def extract_body_content(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    body_content = soup.body
+    if body_content:
+        return str(body_content)
+    return ""
+
+
+def clean_body_content(body_content):
+    soup = BeautifulSoup(body_content, "html.parser")
+
+    for script_or_style in soup(["script", "style"]):
+        script_or_style.extract()
+
+    # Get text or further process the content
+    cleaned_content = soup.get_text(separator="\n")
+    cleaned_content = "\n".join(
+        line.strip() for line in cleaned_content.splitlines() if line.strip()
+    )
+
+    return cleaned_content
+
+
+def split_dom_content(dom_content, max_length=6000):
+    return [
+        dom_content[i : i + max_length] for i in range(0, len(dom_content), max_length)
+    ]
